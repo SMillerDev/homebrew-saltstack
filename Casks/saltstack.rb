babb276af3ff8f29a414b55185e2406f3dbf4664
Sean Molenaar <smillerdev@me.com>
2022-07-07 09:24:39 +0200

    Update saltstack from 3004.1-1 to 3004.2

--- Casks/saltstack.rb
+++ Casks/saltstack.rb
@@ -1,7 +1,7 @@
 cask "saltstack" do
   # Phase 3 support until Sep 30, 2022
-  version "3004.1-1"
-  sha256 "145e8459f754b12e5c1bbc88e1fd30051a1b2c3c51562595fbfcdf61bb3c47de"
+  version "3004.2"
+  sha256 "bc2c32afadde90780ed81353e0572b1b6eb04637987c327cecaf32c37ad9639f"
 
   url "https://repo.saltproject.io/osx/salt-#{version}-py3-x86_64.pkg"
   name "saltstack"
