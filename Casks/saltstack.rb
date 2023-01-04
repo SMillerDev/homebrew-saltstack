2ea479608aa8c69c8202043491a659b091bd3d25
Sean Molenaar <sean@seanmolenaar.eu>
2023-01-04 12:07:32 +0100

    Update saltstack from 3005 to 3005.1

--- Casks/saltstack.rb
+++ Casks/saltstack.rb
@@ -1,7 +1,7 @@
 cask "saltstack" do
   # Phase 3 support until Sep 30, 2022
-  version "3005"
-  sha256 "255eb48d18c75797712bee2286c51d3edadcb7b77b812ecb2ca329469e7b8d3a"
+  version "3005.1"
+  sha256 "6f0a8997fb6c3e257cdc3394e4bacc97fe5b30893ef85224ed93df3cbf094ae9"
 
   url "https://repo.saltproject.io/osx/salt-#{version}-1-py3-x86_64.pkg"
   name "saltstack"
